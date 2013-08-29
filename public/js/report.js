ec72d2c5164466dec9aaef1559ddc7dad2ae428c
Liu Yang <zation1@gmail.com>
2013-08-29 23:52:16 +0800

    add hot logic

--- public/js/report.js
+++ public/js/report.js
@@ -1,34 +1,50 @@
-$(function(){
-	$('table tr td:nth-child(7)').each(function(){$(this).css('display','none')});
-	$('table tr td:nth-child(8)').each(function(){$(this).css('display','none')});
-	$('table tr td:nth-child(9)').each(function(){$(this).css('display','none')});
-	$('#submit').click(function(){
-		$('table tbody td:nth-child(3)').each(function(){
-				$(this).css("color","black");
-				var begin_age = parseInt($('.from.age').val())||0;
-				var end_age = parseInt($('.to.age').val())||100;
-				if(begin_age< parseInt(this.innerHTML) && parseInt(this.innerHTML)<end_age) {
-					$(this).css("color","red");
-				}
-		})
+$(function() {
+	$('table tr td:nth-child(7), table tr td:nth-child(8), table tr td:nth-child(9)').each(function() {
+		$(this).hide();
+	});
 
-		$('table tbody td:nth-child(7)').each(function(){
-			 $(this).css("color","black");
-			if (this.innerHTML==$('#income').val()) $(this).css("color","red");
-		})
+	$('#submit').on('click', function() {
+		var highLightClassName = 'text-danger';
 
-		$('table tbody td:nth-child(7)').each(function(){
-			 $(this).css("color","black");
-			if (this.innerHTML==$('#income').val()) $(this).css("color","red");
-		})
+		$('table tbody td').removeClass(highLightClassName);
+		$('table tbody td').removeClass('text-warning');
+		$('table tbody td').removeClass('text-muted');
+		$('table tbody td:nth-child(3)').each(function() {
+			var begin_age = parseInt($('.from.age').val()) || 0;
+			var end_age = parseInt($('.to.age').val()) || 100;
+			if (begin_age < parseInt(this.innerHTML) && parseInt(this.innerHTML) < end_age) {
+				$(this).addClass(highLightClassName);
+			}
+		});
 
-		$('table tbody td:nth-child(8)').each(function(){
-			 $(this).css("color","black");
-			if (this.innerHTML==$('#risk').val()) $(this).css("color","red");
-		})
+		$('table tbody td:nth-child(7)').each(function() {
+			if (this.innerHTML == $('#income').val()) {
+				$(this).addClass(highLightClassName);
+			}
+		});
 
-		$('table tr td:nth-child(7)').each(function(){$(this).css('display','')});
-		$('table tr td:nth-child(8)').each(function(){$(this).css('display','')});
-		$('table tr td:nth-child(9)').each(function(){$(this).css('display','')});
+		$('table tbody td:nth-child(8)').each(function() {
+			if (this.innerHTML == $('#risk').val()) {
+				$(this).addClass(highLightClassName);
+			}
+		});
+
+		$('table tbody td:nth-child(9)').each(function() {
+			var highLightCount = $(this).parent().find('.' + highLightClassName).length;
+			if (highLightCount > 2) {
+				$(this).html('hot');
+				$(this).addClass(highLightClassName);
+			} else if (highLightCount > 0) {
+				$(this).html('warm');
+				$(this).addClass('text-warning');
+			} else {
+				$(this).html('cold');
+				$(this).addClass('text-muted');
+			}
+		});
+
+		$('table tr td:nth-child(7), table tr td:nth-child(8), table tr td:nth-child(9)').each(function() {
+			$(this).show();
+		});
 	});
 });
